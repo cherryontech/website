ea7fae180acbcd69cf785c410becb1515d1fb18b
novella <novella@novellas-MacBook-Pro.local>
2020-08-15 18:13:50 -0400

    Simplify BaseButton

--- src/components/controls/base/BaseButton.vue
+++ src/components/controls/base/BaseButton.vue
@@ -1,13 +1,5 @@
 <template>
-  <button
-    class="
-      px-10 py-3 rounded-lg border-0
-      min-w-24
-      font-bold cursor-pointer
-      transition-background-color transition-speed-normal transition-timing-function-cubic
-    "
-    v-on="$listeners"
-  >
+  <button class="px-10 py-3 rounded-lg border-0 min-w-24 cursor-pointer">
     <slot />
   </button>
 </template>
