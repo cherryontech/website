385a7fd4dad3c8b99c0108fca125f3c59a56d422
novella <novella@novellas-MacBook-Pro.local>
2020-08-15 18:01:40 -0400

    Create BaseButton component

---
+++ src/components/controls/base/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="
+      px-10 py-3 rounded-lg border-0
+      min-w-24
+      font-bold cursor-pointer
+      transition-background-color transition-speed-normal transition-timing-function-cubic
+    "
+    v-on="$listeners"
+  >
+    <slot />
+  </button>
+</template>
