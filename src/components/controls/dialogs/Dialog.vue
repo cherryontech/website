4e2dd501f18ea310c49695b93ce4a6672bee89e8
novella <novella@novellas-MacBook-Pro.local>
2020-08-29 19:41:17 -0400

    Create and use component with Mailchimp embedded form

--- src/components/controls/dialogs/Dialog.vue
+++ src/components/controls/dialogs/Dialog.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="bg-gray-500 bg-opacity-75 p-12 flex items-center justify-center fixed inset-0">
+  <div
+    class="bg-gray-500 bg-opacity-75 p-12 flex items-center justify-center fixed inset-0"
+  >
     <div class="max-w-xl bg-yellow-400  rounded-lg">
       <div class="flex justify-end">
         <BaseButton @click="$emit('close')">Close</BaseButton>
       </div>
-      <div class="p-12 lg:p-24"> 
-        <h1>Ohh.... Yes! 
-          <br>
+      <div class="p-12 lg:p-24">
+        <h1>
+          Ohh.... Yes!
+          <br />
           We are so excited to share with you what we are cooking.
         </h1>
-        <p>
-          Email submission input here 
-        </p>
+        <Mailchimp />
       </div>
     </div>
   </div>
@@ -19,8 +20,9 @@
 
 <script>
 import BaseButton from "@/components/controls/base/BaseButton.vue";
+import Mailchimp from "@/components/external/Mailchimp.vue";
 
 export default {
-  components: { BaseButton }
-}
+  components: { BaseButton, Mailchimp },
+};
 </script>
---
+++ src/components/external/Mailchimp.vue
@@ -0,0 +1,92 @@
+<template>
+  <!-- Begin Mailchimp Signup Form -->
+  <div id="mc_embed_signup">
+    <form
+      action="https://gmail.us17.list-manage.com/subscribe/post?u=b1231afa3c5fe53c1fa72150f&amp;id=f170e0032e"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_blank"
+      novalidate
+    >
+      <div id="mc_embed_signup_scroll">
+        <div class="mc-field-group">
+          <label for="mce-EMAIL">Email Address </label>
+          <input
+            type="email"
+            value=""
+            name="EMAIL"
+            class="required email"
+            id="mce-EMAIL"
+            v-model="email"
+          />
+        </div>
+        <div class="mc-field-group">
+          <label for="mce-FNAME">First Name </label>
+          <input
+            type="text"
+            value=""
+            name="FNAME"
+            class=""
+            id="mce-FNAME"
+            v-model="firstName"
+          />
+        </div>
+        <div id="mce-responses" class="clear">
+          <div
+            class="response"
+            id="mce-error-response"
+            style="display:none"
+          ></div>
+          <div
+            class="response"
+            id="mce-success-response"
+            style="display:none"
+          ></div>
+        </div>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input
+            type="text"
+            name="b_b1231afa3c5fe53c1fa72150f_f170e0032e"
+            tabindex="-1"
+            value=""
+          />
+        </div>
+        <div class="clear">
+          <input
+            type="submit"
+            value="Subscribe"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            class="button"
+          />
+        </div>
+      </div>
+    </form>
+    {{ email }}
+    {{ firstName }}
+  </div>
+
+  <!--End mc_embed_signup-->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      firstName: "",
+    };
+  },
+  computed: {
+    disableBtn() {
+      const re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/gim;
+      return !re.test(this.email);
+    },
+  },
+};
+</script>
+
+<style></style>
