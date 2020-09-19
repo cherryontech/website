c5a7981b7beb8fc992d9c9877d5bbbb29ddec17b
novella <novella@novellas-MacBook-Pro.local>
2020-09-19 07:02:19 -0400

    Dialog - Add x for close and aria label

--- src/components/controls/dialogs/Dialog.vue
+++ src/components/controls/dialogs/Dialog.vue
@@ -2,16 +2,21 @@
   <div
     class="bg-gray-500 bg-opacity-75 p-12 flex items-center justify-center fixed inset-0"
   >
-    <div class="max-w-xl bg-yellow-400  rounded-lg">
+    <div class="max-w-xl bg-yellow-400 rounded-lg">
       <div class="flex justify-end">
-        <BaseButton @click="$emit('close')">Close</BaseButton>
+        <BaseButton
+          @click="closeDialog()"
+          aria-label="Close Newsletter Signup Box"
+          class="text-5xl"
+          >×</BaseButton
+        >
       </div>
-      <div class="p-12 lg:p-24">
-        <h1>
+      <div class="p-12 lg:p-24 lg:pt-0">
+        <h3 class="text-2xl pb-6">
           Ohh.... Yes!
           <br />
           We are so excited to share with you what we are cooking.
-        </h1>
+        </h3>
         <Mailchimp />
       </div>
     </div>
@@ -24,5 +29,10 @@ import Mailchimp from "@/components/external/Mailchimp.vue";
 
 export default {
   components: { BaseButton, Mailchimp },
+  methods: {
+    closeDialog() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
