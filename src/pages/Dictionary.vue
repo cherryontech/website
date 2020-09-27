9636fae0efc4573bfd21278a30e92803bfcd1282
novella <novella@novellas-MacBook-Pro.local>
2020-09-27 13:44:26 -0400

    Add entry info on the listing page

--- src/pages/Dictionary.vue
+++ src/pages/Dictionary.vue
@@ -5,14 +5,24 @@
       This is our take on a dictionary/glossary of tech terms. It's a little
       different because we try to provide <em>perspectives</em> on each term.
     </p>
+
+    <!-- Print the first page of dictionary entries -->
     <div
       v-for="post in $page.dictionaryposts.edges"
       :key="post.id"
       class="border-purple-900 border-t py-2"
     >
-      <g-link :to="post.node.path" class="text-copy-primary">{{
+      <g-link :to="post.node.path" class="font-bold">{{
         post.node.title
       }}</g-link>
+      <p class="italic">{{ post.node.definition }}</p>
+      <div v-for="(perspective, index) in post.node.perspectives" :key="index">
+        <p>
+          As a <em>{{ perspective.role }}</em
+          >, <span v-if="post.node.title">"{{ post.node.title }}"</span> means
+          {{ perspective.meaning }}.
+        </p>
+      </div>
     </div>
 
     <!-- Pagination -->
@@ -36,6 +46,11 @@ query Dictionaryposts ($page: Int) {
     edges {
       node {
         title
+        definition
+        perspectives {
+          role
+          meaning
+        }
         path
       }
     }
