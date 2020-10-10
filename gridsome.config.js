2e964920cf83cb2189f5f94cb0e84c1ffe76672c
novella <novella@novellas-MacBook-Pro.local>
2020-10-10 17:49:39 -0400

    Remove purgecss

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,13 +4,10 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
 
 const postcssPlugins = [
   tailwind(),
 ]
-
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
   siteName: "Cherry On Tech",
