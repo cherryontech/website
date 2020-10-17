6f250e142bd5eb16bdbc026f92345b40d3c95170
novella <novella@novellas-MacBook-Pro.local>
2020-10-17 13:03:51 -0400

    Add and use vue-gtag as a dependency

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 require('~/main.css')
 
+import VueGtag from "vue-gtag";
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
+  Vue.use(VueGtag, {
+  config: { id: "UA-1234567-1" }
+});
 }
 
 // Require our fonts globally
