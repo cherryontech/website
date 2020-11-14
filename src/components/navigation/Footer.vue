edf8c2ff8e2c455be3ca9c93fb031152f98c521d
novella <novella@novellas-MacBook-Pro.local>
2020-11-14 14:18:04 -0500

    Remove footer borders

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="flex justify-between mx-4 mt-16 border xl:mx-16">
+    <div class="flex justify-between mx-4 mt-16 xl:mx-16">
       <div class="flex">
         <g-link class="flex items-center" to="/">
           <g-image
@@ -9,16 +9,14 @@
             alt="cherry on tech logo"
           />
         </g-link>
-        <div class="border">
-          <nav class="flex flex-col">
-            <g-link class="leading-8" to="/">Home</g-link>
-            <g-link class="leading-8" to="/about/">About Cherry on Tech</g-link>
-            <g-link class="leading-8" to="/dictionary/">Dictionary</g-link>
-            <g-link class="leading-8" to="/blog/">Blog</g-link>
-          </nav>
-        </div>
+        <nav class="flex flex-col">
+          <g-link class="leading-8" to="/">Home</g-link>
+          <g-link class="leading-8" to="/about/">About Cherry on Tech</g-link>
+          <g-link class="leading-8" to="/dictionary/">Dictionary</g-link>
+          <g-link class="leading-8" to="/blog/">Blog</g-link>
+        </nav>
       </div>
-      <div class="flex flex-col border">
+      <div class="flex flex-col">
         <g-image
           width="100"
           src="~/assets/images/cherry--lengua.png"
@@ -27,7 +25,7 @@
         <a href="mailt:cherryontech@gmail.com">cherryontech@gmail.com</a>
       </div>
     </div>
-    <p class="my-8 text-center border">
+    <p class="mt-16 mb-20 text-sm text-center">
       Copyright 2020 Cherry on Tech. All rights reserved.
     </p>
   </div>
