3d88c613d3bcb410b5b59b14636fe0b6c7ca25dd
novella <novella@novellas-MacBook-Pro.local>
2020-11-14 13:54:33 -0500

    Use header component in default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="h-screen">
     <div class="flex flex-col items-stretch justify-between h-full">
-      <header class="flex items-stretch justify-between h-16 bg-purple-900">
-        <g-link class="flex items-center mx-4 xl:mx-16" to="/">
-          <g-image
-            width="44"
-            src="~/assets/images/squareLogoBgPinkMainGreen.png"
-            alt="cherry on tech logo"
-          />
-        </g-link>
-        <nav class="flex items-center flex-grow justify-evenly">
-          <g-link class="px-4 py-2 font-bold text-white" to="/">Home</g-link>
-          <g-link class="px-4 py-2 font-bold text-white" to="/about/"
-            >About</g-link
-          >
-          <g-link class="px-4 py-2 font-bold text-white" to="/dictionary/"
-            >Dictionary</g-link
-          >
-        </nav>
-      </header>
+      <Header />
       <div class="flex-grow p-6">
         <slot />
       </div>
@@ -36,6 +19,14 @@ query {
   }
 }
 </static-query>
+
+<script>
+import Header from "@/components/navigation/Header.vue";
+
+export default {
+  components: { Header },
+};
+</script>
 
 <style scoped>
 nav a.active--exact {
