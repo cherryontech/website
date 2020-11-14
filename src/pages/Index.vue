1c1cf7257c428193d8bf002ab65dfd24a16db240
novella <novella@novellas-MacBook-Pro.local>
2020-11-14 13:56:25 -0500

    Remove frontpage, update abs header hack

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,16 +2,16 @@
   <div
     class="flex flex-col justify-between min-h-screen border-4 border-pink-400"
   >
-    <div class="relative h-12">
-      <Header class="absolute" />
+    <div class="relative h-16">
+      <Header class="absolute" darkMode />
     </div>
     <main
       class="flex flex-col flex-grow bg-pink-500"
       @keydown.esc="showDialog = false"
     >
       <!-- Intro / Outerspace theme -->
-      <div class="-mt-12 bg-cover bg-outerspace" :aria-hidden="hideContentAria">
-        <div class="mt-12">
+      <div class="-mt-16 bg-cover bg-outerspace" :aria-hidden="hideContentAria">
+        <div class="mt-16">
           <g-image
             class="m-auto max-w-screen lg:max-w-lg"
             width="500"
@@ -58,12 +58,11 @@
 <script>
 import BaseButton from "@/components/controls/base/BaseButton.vue";
 import Dialog from "@/components/controls/dialogs/Dialog.vue";
-import Frontpage from "@/layouts/Frontpage.vue";
 import Header from "@/components/navigation/Header.vue";
 import Footer from "@/components/navigation/Footer.vue";
 
 export default {
-  components: { Frontpage, BaseButton, Dialog, Header, Footer },
+  components: { BaseButton, Dialog, Header, Footer },
   metaInfo: {
     title: "Coming soon! Cherry on Tech",
   },
