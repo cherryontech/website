174633c020716564f68f38e15717ff7df0dd7671
novellac <novella@altodev.net>
2020-12-16 23:00:30 -0500

    Layout bio cards

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <div class="grid md:grid-cols-3 md:gap-8">
-      <div v-for="(member, index) in $page.bios.edges" :key="index">
-        <div class="w-full border-4 border-pink-600 h-26">
+    <div class="flex flex-wrap overflow-hidden sm:-mx-2">
+      <div v-for="(member, index) in $page.bios.edges" :key="index" class="flex flex-col w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:w-1/3">
+        <div class="h-40 border-4 border-pink-600">
             <g-image 
               v-if="member.node.memberImage"
               :src="member.node.memberImage" 
               :alt="member.node.title" 
-              class="object-cover h-full"
+              class="object-contain h-full m-auto"
               width="300" 
               height="300"
               quality="75">
