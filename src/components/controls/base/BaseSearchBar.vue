a2f4fd69cb8613cc0b64232eb6f8fc65419bf393
Flor <flordmarshall@gmail.com>
2020-12-25 23:04:05 -0500

    component for searcg bar

---
+++ src/components/controls/base/BaseSearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      id="baseSearch"
+      type="text"
+      :placeholder="placeholderText"
+    />
+    <img :src="iconSrc" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    placeholderText: {
+      type: String,
+      default: "",
+    },
+    iconSrc: {
+      type: String,
+    },
+  },
+};
+</script>
