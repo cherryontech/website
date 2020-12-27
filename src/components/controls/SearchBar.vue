81a37df4a251ab6e854fc0a438002bdd68f8c66a
Flor <flordmarshall@gmail.com>
2020-12-26 20:30:00 -0500

    Move and rename SearchBar

---
+++ src/components/controls/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <BaseInput
+      v-on="$listeners"
+      :value="value"
+      :label="label"
+      hideLabel
+      :placeholderText="placeholderText"
+    />
+  </div>
+</template>
+
+<script>
+import BaseInput from "@/components/controls/base/BaseInput.vue";
+
+export default {
+  components: {
+    BaseInput,
+  },
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "Search",
+    },
+    placeholderText: {
+      type: String,
+      default: "Enter a search term",
+    },
+  },
+};
+</script>
