77121acd45c1b8ff743fb24a20a0a7ea3a398a01
novellac <38117965+novellac@users.noreply.github.com>
2023-10-08 11:26:15 -0400

    chore: Remove obsolete footer items

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="grid gap-6 mx-4 mt-16 sm:gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:mx-16"
+      class="flex flex-wrap items-start justify-between gap-6 mx-4 mt-16 xl:mx-16"
     >
       <!-- Lettermark -->
       <div class="flex">
@@ -15,39 +15,25 @@
       </div>
 
       <!-- Nav -->
-      <nav class="flex flex-col">
+      <nav class="flex flex-col" aria-label="Footer navigation">
         <g-link class="font-semibold leading-10" to="/">Home</g-link>
-        <g-link class="font-semibold leading-10" to="/about/"
-          >About Cherry on Tech</g-link
-        >
         <g-link class="font-semibold leading-10" to="/dictionary/"
           >Dictionary</g-link
         >
-        <g-link class="font-semibold leading-10" to="/blog/">Blog</g-link>
       </nav>
 
-      <!-- Middle free text -->
-      <div class="flex flex-col justify-end">
-        <a class="font-light leading-10" href="https://guides.github.com/"
-          >Learn about Github</a
-        >
-        <g-link class="font-light leading-10" href="/dictionary/"
-          >Our favorite resources</g-link
-        >
-      </div>
-
       <!-- Avatar and Contact -->
-      <div class="flex flex-col lg:col-start-5">
+      <div class="flex flex-col">
         <g-image
           width="100"
           src="~/assets/images/cherry--lengua.png"
-          alt="cherry on tech logo"
+          alt="Cartoon drawing of a blushing, anthropomorphized cherry with big sparkly eyes. The cherry's tongue is blepping a little."
         />
         <a href="mailt:cherryontech@gmail.com">cherryontech@gmail.com</a>
       </div>
     </div>
     <p class="my-4 text-sm text-center md:mb-20 md:mt-16">
-      Copyright 2020 Cherry on Tech. All rights reserved.
+      Copyright 2023 Cherry on Tech. All rights reserved.
     </p>
   </div>
 </template>
